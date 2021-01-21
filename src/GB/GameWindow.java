913b2a277d8e6c229b600a068f181d59ff762c52
Renaon <egorbaturin2013@yandex.ru>
2021-01-21 17:34:50 +0300

    git remote add origin https://github.com/Renaon/Catch_the_drop.git

--- src/GB/GameWindow.java
+++ src/GB/GameWindow.java
@@ -1,5 +1,5 @@
 package GB;
-
+//Задание: сделать, чтоб капля падала по диагонали
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -11,6 +11,10 @@ public class GameWindow extends JFrame {
     private static Image background;
     private static Image gameOver;
     private static Image drop;
+    private static float drop_left = 200;
+    private static float drop_top = -100;
+    private static long last_frame_time;
+    private static float drop_v = 200;
 
     public static void main(String[] args) throws IOException {
         background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
@@ -23,14 +27,20 @@ public class GameWindow extends JFrame {
         game_Window.setLocation(200,100);
         game_Window.setSize(906,478);
         game_Window.setResizable(true);
+        last_frame_time = System.nanoTime();
         GameField game_field = new GameField();
         game_Window.add(game_field);
         game_Window.setVisible(true);
     }
 
     private static void onRepaint(Graphics j){
+        long current_time = System.nanoTime();
+        float delta_time = (current_time - last_frame_time) * 0.000000001f;
+        last_frame_time = current_time;
+
+        drop_top = drop_top+drop_v*delta_time;
         j.drawImage(background,0,0, null);
-        j.drawImage(drop,100,100,null);
+        j.drawImage(drop,(int) drop_left,(int) drop_top,null);
         j.drawImage(gameOver,280,120, null);
     }
 
@@ -39,6 +49,7 @@ public class GameWindow extends JFrame {
         protected void paintComponent(Graphics j){
             super.paintComponent(j);
             onRepaint(j);
+            repaint();
         }
     }
 }
