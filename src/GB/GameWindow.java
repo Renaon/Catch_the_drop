1a4a5f32a0f96c4172d2b67d93e67438885a05ee
Renaon <egorbaturin2013@yandex.ru>
2021-01-21 17:26:11 +0300

    git remote add origin https://github.com/Renaon/Catch_the_drop.git

--- src/GB/GameWindow.java
+++ src/GB/GameWindow.java
@@ -1,19 +1,27 @@
 package GB;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame {
 
     private static GameWindow game_Window;
+    private static Image background;
+    private static Image gameOver;
+    private static Image drop;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        gameOver = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
         //создаем объект окна:
         game_Window = new GameWindow();
         //прерыцваем выполнение программы при закрытии окна
         game_Window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         game_Window.setLocation(200,100);
-        game_Window.setSize(600,478);
+        game_Window.setSize(906,478);
         game_Window.setResizable(true);
         GameField game_field = new GameField();
         game_Window.add(game_field);
@@ -21,8 +29,9 @@ public class GameWindow extends JFrame {
     }
 
     private static void onRepaint(Graphics j){
-        j.fillOval(10,10,200,100);
-        j.drawLine(100,110,250,300);
+        j.drawImage(background,0,0, null);
+        j.drawImage(drop,100,100,null);
+        j.drawImage(gameOver,280,120, null);
     }
 
     private static class GameField extends JPanel implements GB.GameField {
